aeb872201c08af1313b3361a2d0cfc4767ddc4ee
jaym567 <jaymodi0567@gmail.com>
2025-02-01 15:27:40 -0500

    created State machine for wrist

---
+++ src/main/java/com/adambots/subsystems/WristContext.java
@@ -0,0 +1,12 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.adambots.subsystems;
+
+/** Add your docs here. */
+public class WristContext {
+    public double wristCurrentPosition = 0.0;
+    public double wristTargetPosition = 0.0;
+    public double wristMotorSpeed = 0.0;
+}
---
+++ src/main/java/com/adambots/subsystems/WristSubsystem.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.adambots.subsystems;
+
+import com.adambots.actuators.BaseMotor;
+import com.adambots.sensors.BaseAbsoluteEncoder;
+import com.adambots.utils.StateMachine;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class WristSubsystem extends SubsystemBase {
+  /** Creates a new WristSubsystem. */
+  BaseMotor wristMotor;
+  BaseAbsoluteEncoder wristEncoder;
+  // State Machine components
+  private final WristContext context;
+  private final StateMachine<WristContext> stateMachine;
+  private final StateMachine<WristContext>.State scoreState;
+  private final StateMachine<WristContext>.State intakeState;
+
+  // Constants
+  private static final double wristIntakePos = 0.0;
+  private static final double wristScorePos = 0.0;
+  private static final double wristTolerance = 2.0;
+
+  public WristSubsystem(BaseMotor wristMotor, BaseAbsoluteEncoder wristEncoder) {
+    this.wristMotor = wristMotor;
+    this.wristEncoder = wristEncoder;
+
+    // Initialize state machine
+    context = new WristContext();
+    stateMachine = new StateMachine<>(context);
+
+    // Create states with trigger conditions
+    scoreState = stateMachine.addState("Score",
+        () -> (isAtPosition(wristScorePos)) && context.wristTargetPosition == wristScorePos);
+
+    intakeState = stateMachine.addState("Intake", () -> isAtPosition(wristIntakePos) &&
+        (context.wristTargetPosition == wristIntakePos));
+
+    // Define transitions
+    intakeState.addTransition(scoreState, ctx -> {
+      ctx.wristTargetPosition = wristScorePos;
+      ctx.wristMotorSpeed = 0.5;
+    });
+
+    scoreState.addTransition(intakeState, ctx -> {
+      ctx.wristTargetPosition = wristIntakePos;
+      ctx.wristMotorSpeed = 0.5;
+    });
+  }
+
+  private boolean isAtPosition(double position) {
+    return Math.abs(wristEncoder.getAbsolutePositionDegrees() - position) < wristTolerance;
+  }
+
+  // private boolean isMovingTowards(double position) {
+  // double current = armEncoder.getDistance();
+  // return (current < position && context.motorSpeed > 0) ||
+  // (current > position && context.motorSpeed < 0);
+  // }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
