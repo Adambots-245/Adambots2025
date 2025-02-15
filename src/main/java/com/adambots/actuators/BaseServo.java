71a0f46f2a284160564a39426555bd4da57541d9
jaym567 <jaymodi0567@gmail.com>
2025-02-15 11:11:47 -0500

    hnjmko

---
+++ src/main/java/com/adambots/actuators/BaseServo.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.adambots.actuators;
+
+/** Add your docs here. */
+public interface BaseServo {
+
+    void turnCounterclockwise();
+
+    void turnClockwise();
+
+    void stop();
+
+    void setPulseWidth(int pulseWidth);
+
+}
---
+++ src/main/java/com/adambots/actuators/HubServo.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.adambots.actuators;
+
+/** Add your docs here. */
+public class HubServo implements BaseServo{
+
+    @Override
+    public void turnCounterclockwise() {
+        // TODO Auto-generated method stub
+        
+        throw new UnsupportedOperationException("Unimplemented method 'turnCounterclockwise'");
+    }
+
+    @Override
+    public void turnClockwise() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'turnClockwise'");
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+
+    @Override
+    public void setPulseWidth(int pulseWidth) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPulseWidth'");
+    }
+
+    
+}
